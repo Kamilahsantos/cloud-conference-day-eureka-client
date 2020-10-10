1719b2b304f9250a7807dec4e13793b820f83279
kamilah santos <kahsantos89@gmail.com>
2020-10-10 04:19:52 -0300

    eureka client

---
+++ src/test/java/com/demo/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
